4aedfd634458ae28e42bbbb4ac795e2df5dc3848
Stanislav Sedykh <sdsedykh@gmail.com>
2023-04-02 14:40:31 +0300

    release part 2

--- part-2/gameBoard.js
+++ part-2/gameBoard.js
@@ -6,11 +6,24 @@ class GameBoard {
   }
 
   terrainMap() {
-    // напишите этот метод сами
+    const arr = [];
+    for (let i = 0; i < this.size; i += 1) {
+      const subArr = [];
+      for (let j = 0; j < this.size; j += 1) {
+        subArr.push(this.terrainHelper.letterToTerrain(this.terrainList[i * this.size + j]));
+      }
+      arr.push(subArr);
+    }
+    return arr;
   }
 
-  difficultyOfJourney() {
-    // напишите этод метод сами
+  difficultyOfJourney(journey) {
+    let sum = 0;
+    const map = this.terrainMap();
+    journey.forEach((element) => {
+      sum += this.terrainHelper.scoreDifficulty(map[element[0]][element[1]]);
+    });
+    return sum;
   }
 }
 
