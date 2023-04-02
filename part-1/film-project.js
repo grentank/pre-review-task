4e758c5d9f6f99ce41a036a0d0b999abf0e29c1d
Arthur <madoyan_2017@bk.ru>
2023-04-02 20:20:45 +0300

    1

--- part-1/film-project.js
+++ part-1/film-project.js
@@ -1,6 +1,25 @@
 class FilmProject {
   constructor(args = {}) {
     this.title = args.title;
+    this.director = args.director;
+    this.cast = args.cast;
+    this.castingBudget = args.castingBudget;
+  }
+
+  remainingBudget() {
+    const sumOfSalary = this.cast.reduce((a, b) => a.salary + b.salary);
+    return this.castingBudget - sumOfSalary;
+  }
+
+  addActor(actor) {
+   if (actor.salary > this.castingBudget) {
+    return false
+   }
+   return this.cast.push(actor)
+  }
+
+  headliners(){
+    return this.cast.filter((e) => e.popularityRating > 8)
   }
 }
 
--- part-3/namesList.js
+++ part-3/namesList.js
@@ -1,11 +1,17 @@
 const fs = require('fs/promises');
 const fetchUsers = require('./fetchUsers');
 
-function fetchNames() {}
+async function fetchNames(){
+return (await fetchUsers()).map((e) => e.name)
+}
 
-function saveNames(names) {}
+async function saveNames(names) {
+  return fs.writeFile('./names.txt', names.join('\n'), 'utf-8');
+}
 
-function getName(count) {}
+async function getName(el) {
+  return fs.readFile('./names.txt', 'utf-8').then((data) => data.split('\n')[el - 1]);
+}
 
 module.exports = {
   fetchNames,
