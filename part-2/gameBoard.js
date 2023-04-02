3c793df67b3303f50392681f90f9b54c6b2783ab
Evgeniy <shichkov97@gmail.com>
2023-04-02 18:54:53 +0300

    homeWork

--- part-2/gameBoard.js
+++ part-2/gameBoard.js
@@ -6,12 +6,23 @@ class GameBoard {
   }
 
   terrainMap() {
-    // напишите этот метод сами
+    const arr = [];
+   const arrey = this.terrainList.map((el)=> this.terrainHelper.letterToTerrain(el))
+    for(let i = 0; i < this.terrainList.length; i+= this.size) {
+      arr.push(arrey.slice(i, i+ this.size))
+    }
+    return arr
   }
 
-  difficultyOfJourney() {
-    // напишите этод метод сами
+  difficultyOfJourney(arr) {
+   const dosk =  this.terrainMap()
+   let count = 0
+   for(let i = 0; i < arr.length; i+=1){
+   count += this.terrainHelper.scoreDifficulty(dosk[arr[i][0]][arr[i][1]])
+   
+   
   }
+  return count
 }
-
+}
 module.exports = GameBoard;
